c100a8920d6bbee4f2583208f023410c6dbf80b1
Georg <georgiy.nikitin@gmail.com>
2020-04-23 13:20:35 +0300

    Создан src/contollers/moviecontoller.js

---
+++ src/contollers/moviecontoller.js
@@ -0,0 +1,16 @@
+
+import FilmCardComponent from "../components/film-card";
+import FilmDetailsComponent from "../components/film-details";
+import {isEscEvent} from "../utils/common";
+import {render, remove, append, RenderPosition} from "../utils/render";
+
+export default class MovieController {
+  constructor(container) {
+    this._container = container;
+  }
+
+  render(film) {
+
+  }
+}
+
