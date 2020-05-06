fdde54aa892f2ce3e1bbcd9f7a0e15a8dd42ada2
Georg <georgiy.nikitin@gmail.com>
2020-05-06 22:32:33 +0300

    Перенес получение модели комментариев в page-controll

--- src/main.js
+++ src/main.js
@@ -1,28 +1,23 @@
-import {generateFilms, comments} from "./mock/films";
+import {generateFilms} from "./mock/films";
 import {render, RenderPosition} from "./utils/render";
 
 import PageController from "./contollers/page-controller";
 import ProfileComponent from "./components/profile";
 import FooterComponent from "./components/footer";
 import FilmsModel from "./models/movies";
-import CommentsModel from "./models/comments";
 
-
-const COUNT_FILMS = 12;
+const COUNT_FILMS = 17;
 
 const films = generateFilms(COUNT_FILMS);
 
 const filmsModel = new FilmsModel();
 filmsModel.setFilms(films);
 
-const commentsModel = new CommentsModel();
-commentsModel.setComments(comments);
-
 const siteHeaderElement = document.querySelector(`.header`);
 render(siteHeaderElement, new ProfileComponent(films), RenderPosition.BEFOREEND);
 
 const siteMainElement = document.querySelector(`.main`);
-const pageController = new PageController(siteMainElement, filmsModel, commentsModel);
+const pageController = new PageController(siteMainElement, filmsModel);
 pageController.render();
 
 const footerElement = document.querySelector(`.footer`);
