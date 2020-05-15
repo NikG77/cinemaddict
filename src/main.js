8d38629001104c4f42aedd72c11df5b5e5eec419
Georg <georgiy.nikitin@gmail.com>
2020-05-15 17:02:16 +0300

    Рефакторинг main.js

--- src/main.js
+++ src/main.js
@@ -1,16 +1,15 @@
-import {generateFilms} from "./mock/films";
-import {render, RenderPosition} from "./utils/render";
-
-import PageController from "./contollers/page-controller";
-import ProfileComponent from "./components/profile";
-import FooterComponent from "./components/footer";
+import FilmsComponent from "./components/films";
 import FilmsModel from "./models/movies";
 import FilterController from "./contollers/filter-controller";
+import FooterComponent from "./components/footer";
+import Navigation from "./components/navigation";
+import PageController from "./contollers/page-controller";
+import ProfileComponent from "./components/profile";
 import SortController from "./contollers/sort";
 import StatisticsComponent from "./components/statistics";
-import FilmsComponent from "./components/films";
-import Navigation from "./components/navigation";
+import {generateFilms} from "./mock/films";
 import {MenuItem} from "./const";
+import {render, RenderPosition} from "./utils/render";
 
 
 const COUNT_FILMS = 17;
@@ -21,10 +20,11 @@ const filmsModel = new FilmsModel();
 filmsModel.setFilms(films);
 
 const siteHeaderElement = document.querySelector(`.header`);
+const siteMainElement = document.querySelector(`.main`);
+const footerStatisticsElement = document.querySelector(`.footer__statistics`);
+
 const profileComponent = new ProfileComponent(films);
 render(siteHeaderElement, profileComponent, RenderPosition.BEFOREEND);
-
-const siteMainElement = document.querySelector(`.main`);
 
 const navigationComponent = new Navigation();
 render(siteMainElement, navigationComponent, RenderPosition.BEFOREEND);
@@ -33,26 +33,19 @@ const siteNavigationElements = siteMainElement.querySelector(`nav`);
 const filterController = new FilterController(siteNavigationElements, filmsModel);
 filterController.render();
 
-
 const sortController = new SortController(siteMainElement, filmsModel);
 sortController.render();
 
 const filmsComponent = new FilmsComponent();
 render(siteMainElement, filmsComponent, RenderPosition.BEFOREEND);
 
-
 const pageController = new PageController(filmsComponent, filmsModel);
 pageController.render();
-pageController.hide();
-
 
 const statisticsComponent = new StatisticsComponent(filmsModel, profileComponent.getRating());
 render(siteMainElement, statisticsComponent, RenderPosition.BEFOREEND);
-// statisticsComponent.hide();
+statisticsComponent.hide();
 
-
-const footerElement = document.querySelector(`.footer`);
-const footerStatisticsElement = footerElement.querySelector(`.footer__statistics`);
 render(footerStatisticsElement, new FooterComponent(films.length), RenderPosition.BEFOREEND);
 
 navigationComponent.setNavigationChangeHandler((menuItem) => {
@@ -68,7 +61,6 @@ navigationComponent.setNavigationChangeHandler((menuItem) => {
       sortController.reset();
       sortController.show();
       pageController.show();
-
       break;
   }
 
